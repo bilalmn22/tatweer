ec4bc7506f689997e384de28ec9e859a843d2cf8
bilalmn22 <bilalmamoun2004@gmail.com>
2025-02-08 17:36:06 +0100

    correct vercel bugs

--- app/ui/tracking/latest-deliveries.jsx
+++ app/ui/tracking/latest-deliveries.jsx
@@ -1,18 +1,22 @@
 "use client";
-import "./latest-deliveries.css"
-import { useState } from "react";
+import "./latest-deliveries.css";
+import { Suspense, useState } from "react";
 import DeliveryBox from "./delivery-box";
 function LatestDeliveries() {
   const [selected, setSelected] = useState(0);
   return (
-    <div id="tracking-latest-deliveries"  className="overflow-y-scroll grow pr-4 mt-3">
+    <div
+      id="tracking-latest-deliveries"
+      className="overflow-y-scroll grow pr-4 mt-3"
+    >
       {[...Array(8)].map((_, index) => (
-        <DeliveryBox
-          selected={selected}
-          setSelected={setSelected}
-          index={index}
-          key={index}
-        />
+        <Suspense index={index}>
+          <DeliveryBox
+            selected={selected}
+            setSelected={setSelected}
+            key={index}
+          />
+        </Suspense>
       ))}
     </div>
   );
