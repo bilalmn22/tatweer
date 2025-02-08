514beba313bb846ba2519077652fab1ab6348b8a
bilalmn22 <bilalmamoun2004@gmail.com>
2025-02-08 17:30:17 +0100

    add suspnse compenet to tracking page

--- app/tracking/page.jsx
+++ app/tracking/page.jsx
@@ -3,12 +3,15 @@ import LatestDeliveries from "../ui/tracking/latest-deliveries";
 import TrackingMap from "../ui/tracking/map";
 import DeliveryInfo from "../ui/tracking/delivery-info";
 import { poppins } from "../ui/fonts";
+import { Suspense } from "react";
 
 export default async function Tracking() {
   return (
     <div className={`w-full flex max-h-[980px]  gap-6 ${poppins.className}`}>
       <div className="min-w-[300px] flex flex-col   ">
-        <Search placeholder={"Search Deliveries by ID ..."} />
+        <Suspense>
+          <Search placeholder={"Search Deliveries by ID ..."} />
+        </Suspense>
         <LatestDeliveries />
       </div>
       <div className=" grow  flex flex-col  ">
