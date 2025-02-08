642b943084cebf8f38a80c7eb6578e8900be0bbb
bilalmn22 <bilalmamoun2004@gmail.com>
2025-02-08 17:28:14 +0100

    add suspense to shipping page

--- app/shipement/page.jsx
+++ app/shipement/page.jsx
@@ -2,15 +2,22 @@ import Table from "../ui/deliveries/table";
 import TablePagination from "../ui/deliveries/pagination";
 import Search from "../ui/Search";
 import { poppins } from "../ui/fonts";
+import { Suspense } from "react";
 
 export default async function Page() {
   return (
     <main className={poppins.className}>
       <div className=" min-h-[50px]  flex flex-wrap gap-4 justify-between items-center ">
+        <Suspense>
         <Search placeholder={"Search deliveries here ..."} />
+
+        </Suspense>
       </div>
       <Table />
+      <Suspense>
       <TablePagination />
+
+      </Suspense>
     </main>
   );
 }
