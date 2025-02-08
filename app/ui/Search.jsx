1aaa99d9b0de56fed8f27a39fd89085b96a5b1bb
bilalmn22 <bilalmamoun2004@gmail.com>
2025-02-08 17:24:43 +0100

    correct logs

--- app/ui/Search.jsx
+++ app/ui/Search.jsx
@@ -4,6 +4,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import InputBase from "@mui/material/InputBase";
 import { useDebouncedCallback } from "use-debounce";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { Suspense } from "react";
 function Search({ placeholder }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
@@ -33,19 +34,22 @@ function Search({ placeholder }) {
       <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
         <SearchIcon />
       </IconButton>
-      <InputBase
-        style={{
-          boxShadow:
-            "linear-gradient(0deg, #fff 0%, #fff 100%),linear-gradient(0deg,rgba(42, 89, 254, 0.3) 0%,rgba(42, 89, 254, 0.3) 100%),#000",
-        }}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        className="text-black text-lg  font-medium"
-        defaultValue={searchParams.get("query")?.toString()}
-        sx={{ ml: 1, flex: 1 }}
-        placeholder={placeholder}
-      />
+      <Suspense>
+        <InputBase
+          style={{
+            boxShadow:
+              "linear-gradient(0deg, #fff 0%, #fff 100%),linear-gradient(0deg,rgba(42, 89, 254, 0.3) 0%,rgba(42, 89, 254, 0.3) 100%),#000",
+          }}
+          onChange={(e) => {
+            handleSearch(e.target.value);
+          }}
+          className="text-black text-lg  font-medium"
+          defaultValue={searchParams.get("query")?.toString()}
+          sx={{ ml: 1, flex: 1 }}
+          placeholder={placeholder}
+        />
+      </Suspense>
+
       <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
     </Paper>
   );
